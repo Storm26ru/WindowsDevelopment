7ceb0a5e6dc7b1da614aac0bbb60d54d8d8a44bb
Storm <Storm@STORMSSD>
2024-10-28 23:41:19 +0300

    ComboBox done

--- WinApi_start/ComboBox/main.cpp
+++ WinApi_start/ComboBox/main.cpp
@@ -1,23 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include"resource.h"
+#include<stdio.h>
 
-
-CONST CHAR* init_values[] = { "This","is","my","first","Combo", "Box" };
+CONST CHAR* init_values[] = { "This","is","my","Combo","Box" };
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR CmdLine, INT CmdShow)
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
 {
 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
 
-
 	return 0;
 }
 
-
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch(uMsg)
+	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
@@ -26,25 +25,31 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)init_values[i]);
 		}
+
 	}
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-		case IDOK:
+		case IDOK: 
 		{
-			MessageBox(hWnd, "Нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
+			HWND hCombo = GetDlgItem(hWnd, IDC_COMBO1);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_messeg[SIZE]{};
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			sprintf(sz_messeg, "Вы выбрали пункт №%i со значением \"%s\".", i, sz_buffer);
+			MessageBox(hWnd, sz_messeg, "Info", MB_OK | MB_ICONINFORMATION); break;
 		}
-		case IDCANCEL:
-			EndDialog(hWnd, 0); break;
+		case IDCANCEL: EndDialog(hWnd, 0); break;
 		}
+
 		break;
 	case WM_CLOSE:
 		EndDialog(hWnd, 0);
 		break;
 	}
-
 	return FALSE;
-
-
 }
+
