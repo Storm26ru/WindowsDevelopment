acfe7d5c916742824c63dfd7aa5cd8d10de5b1c1
Storm <Storm@STORMSSD>
2024-11-19 00:57:14 +0300

    start

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -28,13 +28,34 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		MessageBox(NULL, "Class registration failed", "", MB_OK | MB_ICONERROR);
 		return 0;
 	}
+	HWND hWnd = CreateWindowEx(NULL, g_sz_WINDOW_CLASS, g_sz_WINDOW_CLASS, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (hWnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
 
 INT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-
-
+	switch (uMsg)
+	{
+	case WM_CREATE: break;
+	case WM_COMMAND: break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	case WM_CLOSE: DestroyWindow(hWnd); break;
+	default: return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 	return 0;
 }
