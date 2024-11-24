58e6fdef641fb07da829ca5eaa309ab8c9360200
Storm <Storm@STORMSSD>
2024-11-24 10:46:52 +0300

    done

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,11 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<stdio.h>
+
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Main Window";
 INT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+HICON hSizeNS,hArrow,hSizeNESW,hSizeNWSE,hSizeWE;
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
 {
+	INT Screen_widht = GetSystemMetrics(SM_CXSCREEN);
+	INT Screen_hight = GetSystemMetrics(SM_CYSCREEN);
+
+	hSizeNS = (HICON)LoadImage(hInstance, "ico\\SizeNS.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
+	hArrow = (HICON)LoadImage(hInstance, "ico\\Arrow.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
+	hSizeWE = (HICON)LoadImage(hInstance, "ico\\SizeWE.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
+	hSizeNWSE = (HICON)LoadImage(hInstance, "ico\\SizeNWSE.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
+	hSizeNESW = (HICON)LoadImage(hInstance, "ico\\SizeNESW.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
 
 	WNDCLASSEX wClass;
 	ZeroMemory(&wClass, sizeof(wClass));
@@ -13,11 +24,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;
 	wClass.cbSize = sizeof(wClass);
-	
-	wClass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);//https://learn.microsoft.com/ru-ru/windows/win32/menurc/about-icons
-	wClass.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	//wClass.hIcon =LoadIcon(hInstance,MAKEINTRESOURCE(IDI_ICON1));
+	//wClass.hIconSm =LoadIcon(hInstance,MAKEINTRESOURCE(IDI_ICON2));
+	wClass.hIcon =(HICON)LoadImage(hInstance,"ico\\web.ico",IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wClass.hIconSm = (HICON)LoadImage(hInstance, "ico\\burn.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW); //https://learn.microsoft.com/ru-ru/windows/win32/menurc/about-cursors
+	wClass.hCursor = (HCURSOR)LoadImage(hInstance, "ico\\Arrow.ani", IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE);
+	//LoadCursor(hInstance, IDC_ARROW); 
 
 	wClass.lpszClassName = g_sz_WINDOW_CLASS;
 	wClass.lpszMenuName = NULL;
@@ -28,8 +41,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		MessageBox(NULL, "Class registration failed", "", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	HWND hWnd = CreateWindowEx(NULL, g_sz_WINDOW_CLASS, g_sz_WINDOW_CLASS, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	HWND hWnd = CreateWindowEx(NULL, g_sz_WINDOW_CLASS, g_sz_WINDOW_CLASS, WS_OVERLAPPEDWINDOW, Screen_widht/8, Screen_hight/8,
+		Screen_widht*.75, Screen_hight*.75, NULL, NULL, hInstance, NULL);
 	if (hWnd == NULL)
 	{
 		MessageBox(NULL, "Window creation failed", "", MB_OK | MB_ICONERROR);
@@ -52,6 +65,38 @@ INT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE: break;
+	case WM_SETCURSOR:
+		switch (LOWORD(lParam))
+		{
+		case HTTOPRIGHT:
+		case HTBOTTOMLEFT:
+			SetCursor(hSizeNESW); break;
+		case HTTOPLEFT:
+		case HTBOTTOMRIGHT:
+			SetCursor(hSizeNWSE); break;
+		case HTLEFT:
+		case HTRIGHT:
+			SetCursor(hSizeWE); break;
+		case HTTOP:
+		case HTBOTTOM:
+			SetCursor(hSizeNS); break;
+		default:
+			SetCursor(hArrow); 
+		}break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		//WINDOWINFO wInfo;
+		//wInfo.cbSize = sizeof(wInfo);
+		//GetWindowInfo(hWnd, &wInfo);
+		RECT rect;
+		GetWindowRect(hWnd, &rect);
+		CONST INT SIZE = 256;
+		CHAR wt_bufer[SIZE]{};
+		sprintf(wt_bufer, " %s  Size - %ix%i  Position - x%i;y%i",g_sz_WINDOW_CLASS,rect.right-rect.left,
+				rect.bottom-rect.top,rect.left,rect.top);
+		SendMessage(hWnd, WM_SETTEXT, 0, (LPARAM)wt_bufer); break;
+	}	
 	case WM_COMMAND: break;
 	case WM_DESTROY: PostQuitMessage(0); break;
 	case WM_CLOSE: DestroyWindow(hWnd); break;
