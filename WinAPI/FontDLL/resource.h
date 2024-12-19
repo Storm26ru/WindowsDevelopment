84f164d284b32f16c00efe151c359cd444fd58c5
Storm <Storm@STORMSSD>
2024-12-20 01:39:58 +0300

    Fontdll done

---
+++ WinAPI/FontDLL/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+#define IDF_CALC_2                  100
+#define IDF_POCKET_CALCULATOR       101
+#define IDF_MOSCOW2024              102
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
