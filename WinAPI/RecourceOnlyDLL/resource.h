2365a41c4b7f6548fce3d899d257eae68fc6dbf5
Александр <@STORM>
2024-12-20 15:50:11 +0300

    done

--- WinAPI/RecourceOnlyDLL/resource.h
+++ WinAPI/RecourceOnlyDLL/resource.h
@@ -2,26 +2,26 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDB_BUTTON_0                    100
-#define IDB_BUTTON_1                    101
-#define IDB_BUTTON_2                    102
-#define IDB_BUTTON_3                    103
-#define IDB_BUTTON_4                    104
-#define IDB_BUTTON_5                    105
-#define IDB_BUTTON_6                    106
-#define IDB_BUTTON_7                    107
-#define IDB_BUTTON_8                    108
-#define IDB_BUTTON_9                    109
-#define IDB_BUTTON_POINT                110
-#define IDB_BUTTON_PLUS                 111
-#define IDB_BUTTON_MINUS                112
-#define IDB_BUTTON_ASTER                113
-#define IDB_BUTTON_SLASH                114
-#define IDB_BUTTON_BSP                  115
-#define IDB_BUTTON_CLR                  116
-#define IDB_BUTTON_EQUAL                117
-#define IDR_FONT1                       119
-#define IDI_ICON1                       200
+#define IDB_BUTTON_0                    1000
+#define IDB_BUTTON_1                    1001
+#define IDB_BUTTON_2                    1002
+#define IDB_BUTTON_3                    1003
+#define IDB_BUTTON_4                    1004
+#define IDB_BUTTON_5                    1005
+#define IDB_BUTTON_6                    1006
+#define IDB_BUTTON_7                    1007
+#define IDB_BUTTON_8                    1008
+#define IDB_BUTTON_9                    1009
+#define IDB_BUTTON_POINT                1010
+#define IDB_BUTTON_PLUS                 1011
+#define IDB_BUTTON_MINUS                1012
+#define IDB_BUTTON_ASTER                1013
+#define IDB_BUTTON_SLASH                1014
+#define IDB_BUTTON_BSP                  1015
+#define IDB_BUTTON_CLR                  1016
+#define IDB_BUTTON_EQUAL                1017
+#define IDR_FONT1                       1019
+#define IDI_ICON1                       106
 
 // Next default values for new objects
 // 
